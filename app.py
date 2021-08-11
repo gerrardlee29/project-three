c09d9f13956ca48c3887edc98bfc1e7bc8189685
gerrardlee29 <gerrardlee29@gmail.com>
2021-08-11 00:16:17 +0000

    new file

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-import os
 from flask import Flask, render_template
 
 
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('base.html')
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+    app.run(debug=True)
